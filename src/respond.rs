a14140049a6189ff1932db3f1c0153ae1dd53699
Hasan Ali <hasan@hasali.dev>
2021-09-13 22:13:19 +0100

    Add respond_file helper to serve static files

--- src/respond.rs
+++ src/respond.rs
@@ -1,14 +1,29 @@
-use headers::{ContentType, Header};
+use std::io::SeekFrom;
+use std::ops::Bound;
+use std::path::Path;
+
+use async_trait::async_trait;
+use headers::{AcceptRanges, ContentLength, ContentRange, ContentType, Header, Range};
 use hyper::{Body, StatusCode};
 use serde::Serialize;
+use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncSeekExt};
+use tokio_util::codec::{BytesCodec, FramedRead};
 
 use crate::{Request, Response};
 
+#[async_trait]
 pub trait RespondRequestExt {
     fn ok(&mut self) -> Respond;
     fn respond<R: Into<Response>>(&mut self, res: R) -> Respond;
+
+    async fn respond_file(
+        &mut self,
+        path: impl AsRef<Path> + Send + 'async_trait,
+    ) -> std::io::Result<Respond>;
 }
 
+#[async_trait]
 impl RespondRequestExt for Request {
     fn ok(&mut self) -> Respond {
         self.respond(StatusCode::OK)
@@ -16,6 +31,59 @@ impl RespondRequestExt for Request {
 
     fn respond<R: Into<Response>>(&mut self, res: R) -> Respond {
         Respond(self.set_res(res.into()))
+    }
+
+    async fn respond_file(
+        &mut self,
+        path: impl AsRef<Path> + Send + 'async_trait,
+    ) -> std::io::Result<Respond> {
+        let mime = mime_guess::from_path(&path).first_or("video/*".parse().unwrap());
+        let mut file = File::open(path).await?;
+
+        let length = file.metadata().await?.len();
+        let range = self.header::<Range>().and_then(|range| range.iter().next());
+
+        let mut res = Response::ok()
+            .with_header(AcceptRanges::bytes())
+            .with_header(ContentLength(length));
+
+        match range {
+            Some((from, to)) => {
+                let from = match from {
+                    Bound::Included(n) => n,
+                    Bound::Excluded(n) => n + 1,
+                    Bound::Unbounded => 0,
+                };
+
+                let to = match to {
+                    Bound::Included(n) => n,
+                    Bound::Excluded(n) => n - 1,
+                    Bound::Unbounded => length - 1,
+                };
+
+                file.seek(SeekFrom::Start(from)).await?;
+
+                let total_length = length;
+                let length = u64::min(length - from, to - from + 1);
+                let reader = file.take(length);
+                let stream = FramedRead::new(reader, BytesCodec::new());
+                let body = Body::wrap_stream(stream);
+                let range = from..=from + length - 1;
+
+                res.set_status(StatusCode::PARTIAL_CONTENT);
+                res.set_header(ContentRange::bytes(range, total_length).unwrap());
+                res.set_header(ContentType::from(mime));
+                res.set_body(body);
+            }
+            None => {
+                let stream = FramedRead::new(file, BytesCodec::new());
+                let body = Body::wrap_stream(stream);
+
+                res.set_body(body);
+            }
+        }
+
+        Ok(self.respond(res))
     }
 }
 
