fcb283996077da1e725053c412bdaa44cf7b11bf
Hasan Ali <hasan@hasali.dev>
2021-10-16 21:27:23 +0100

    Re-export serde_qs::Error

--- src/query.rs
+++ src/query.rs
@@ -2,12 +2,14 @@ use serde::de::DeserializeOwned;
 
 use crate::Request;
 
+pub type QueryError = serde_qs::Error;
+
 pub trait QueryRequestExt {
-    fn query<T: DeserializeOwned>(&self) -> Result<T, serde_qs::Error>;
+    fn query<T: DeserializeOwned>(&self) -> Result<T, QueryError>;
 }
 
 impl QueryRequestExt for Request {
-    fn query<T: DeserializeOwned>(&self) -> Result<T, serde_qs::Error> {
+    fn query<T: DeserializeOwned>(&self) -> Result<T, QueryError> {
         serde_qs::from_str(self.uri().query().unwrap_or(""))
     }
 }
